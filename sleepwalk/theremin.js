cb3b5b909f78acf247ace442aecca4b5404b2783
WuchuJhin <82705831+WuchuJhin@users.noreply.github.com>
2021-04-17 21:25:33 +0300

    Update theremin.js

--- sleepwalk/theremin.js
+++ sleepwalk/theremin.js
@@ -1,7 +1,7 @@
 document.getElementById('button').onclick =  function CheckCode() {
     let code = document.getElementById('code').value;
     if (code == '51124716791918') {
-        alert('Congratulation: ')
+        alert('Congratulation: 51124716791918')
     } else {
         alert('I Guess that you will have to retry...')
     }
