1b6deb9686787b99c1056c9e0de64be7c3c55333
WuchuJhin <82705831+WuchuJhin@users.noreply.github.com>
2021-04-17 21:17:15 +0300

    Update prepare_to_cry.js

--- prepare_to_cry/prepare_to_cry.js
+++ prepare_to_cry/prepare_to_cry.js
@@ -1,7 +1,7 @@
 document.getElementById('button').onclick =  function CheckCode() {
     let password =document.getElementById('password').value;
     if (password == '8xty0pf4ZE') {
-        alert('Well done, here is the next link:')
+        alert('Well done, here is the next link: https://justapuzzle.netlify.app/back_to_the_past/break.html')
     } else {
         alert('ERROR, You Failed... I\'m desapointed')
     }
