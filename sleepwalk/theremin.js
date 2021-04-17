893ebc5fbaf9e4cc0e88eb6bfada08eee6952c8a
WuchuJhin <82705831+WuchuJhin@users.noreply.github.com>
2021-04-17 21:26:25 +0300

    Update theremin.js

--- sleepwalk/theremin.js
+++ sleepwalk/theremin.js
@@ -1,7 +1,7 @@
 document.getElementById('button').onclick =  function CheckCode() {
     let code = document.getElementById('code').value;
     if (code == '51124716791918') {
-        alert('Congratulation: 51124716791918')
+        alert('Congratulation: https://justapuzzle.netlify.app/b_side/end_screen/end_screen.html')
     } else {
         alert('I Guess that you will have to retry...')
     }
